70ad2c88fac7ae8308e78290eeb389cbceb0a21e
A-fethi <fethiabderrahmane1@gmail.com>
2023-10-06 08:49:17 -0700

    Enhacement

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,26 +3,30 @@ from fabric.api import *
 import os.path
 
 
-env.hosts = ["52.3.250.108", "100.25.14.73"]
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
+env.hosts = ["54.160.99.253", "100.25.14.73"]
 
 
 def do_deploy(archive_path):
     """A script that distributes an archive to web servers"""
-    if not os.path.isfile(archive_path):
-        return False
-    file_path = os.path.basename(archive_path)
-    name = os.path.splitext(file_path)[0]
     try:
-        put(archive_path, "/tmp/{}".format(file_path))
-        run("mkdir -p /data/web_static/releases/{}".format(name))
+        if not os.path.exists(archive_path):
+            return False
+        file_path = os.path.basename(archive_path)
+        name = os.path.splitext(file_path)[0]
+        put(archive_path, "/tmp/")
+        run("rm -rf /data/web_static/releases/{}".format(name))
+        run("mkdir -p /data/web_static/releases/{}/".format(name))
         run("tar -xzf /tmp/{} -C /data/web_static/releases/{}"
             .format(file_path, name))
         run("rm /tmp/{}".format(file_path))
+        run("mv /data/web_static/releases/{0}/web_static/* \
+            /data/web_static/releases/{0}/".format(name))
+        run("rm -rf /data/web_static/releases/{}/web_static"
+            .format(name))
         run("rm -rf /data/web_static/current")
         run("ln -sf /data/web_static/releases/{}/ /data/web_static/current"
             .format(name))
+        print("New version deployed!")
         return True
     except Exception:
         return False
